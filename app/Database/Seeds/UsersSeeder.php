9b592dfd8dcb5db89c47b530cc31dba5283c5c17
patian <santo.patian@gmail>
2022-01-24 22:12:01 +0700

    change app database, add user table migration, add user seeder

---
+++ app/Database/Seeds/UsersSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class UsersSeeder extends Seeder
+{
+    public function run()
+    {
+        //
+        $data = [
+            'username'  => 'Admin',
+            'name'      => 'Santo',
+            'email'     => 'admin@example.com',
+            'password'  => '$2y$10$sOKww3LkoNzBtuW5SKxcJOVcY5eN3L1UsTMZpzgWLDd5MfiZ2mmNe',
+            'status'    => 'Active',
+            'level'     => 'Admin',
+        ];
+    }
+}
